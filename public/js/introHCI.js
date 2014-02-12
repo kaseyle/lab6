cc63543022989ae1ab428176740d98cc58d6127d
vagrant <vagrant@precise32.(none)>
2014-02-12 23:10:45 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,13 +26,33 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, getProject);
+
+	console.log("User clicked on link: " + "/project/" + idNumber);
 	console.log("User clicked on project " + idNumber);
 }
 
+function getProject(result) {
+	console.log(result);
+	var html = "<image class='detailsImage' src='" + result["image"] + "'>";
+	html += "<h3>" + result["date"] + "</h3>";
+	html += result["summary"];
+	$( "#project" + result["id"] + " .details" ).html(html);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColor);
+}
+
+function changeColor(result) {
+	var colors = result["colors"]["hex"];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
